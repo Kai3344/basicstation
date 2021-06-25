cd095ae5772f63fb95f6539c35a011b53bb7645c
Xose Pérez <xose.perez@gmail.com>
2021-06-25 15:49:46 +0200

    Building loragw v2

--- start.sh
+++ start.sh
@@ -16,18 +16,20 @@ TAG=$(curl -sX POST \
 -H "Authorization: Bearer $BALENA_API_KEY" \
 --data "{ \"device\": \"$ID\", \"tag_key\": \"$TAG_KEY\", \"value\": \"$TTN_EUI\" }" > /dev/null)
 
-
-
 if [ -z ${MODEL} ] ;
  then
-    echo -e "\033[91mWARNING: MODEL variable not set.\n Set the model of the gateway you are using (SX1301 or SX1302).\033[0m"
+    echo -e "\033[91mWARNING: MODEL variable not set.\n Set the model of the gateway you are using (SX1301, SX1302 or SX1303).\033[0m"
     balena-idle
  else
+    MODEL=${MODEL^^}
     echo "Using MODEL: $MODEL"
-    if [ "$MODEL" = "SX1301" ] || [ "$MODEL" = "RAK2245" ] || [ "$MODEL" = "iC880a" ];then
+    if [ "$MODEL" = "SX1301" ] || [ "$MODEL" = "RAK833" ] || [ "$MODEL" = "RAK2245" ] || [ "$MODEL" = "RAK2247" ] || [ "$MODEL" = "IC880A" ];then
         ./start_sx1301.sh
     fi
     if [ "$MODEL" = "SX1302" ] || [ "$MODEL" = "RAK2287" ];then
+        ./start_sx1302.sh
+    fi
+    if [ "$MODEL" = "SX1303" ] || [ "$MODEL" = "RAK5146" ];then
         ./start_sx1302.sh
     fi
 fi
